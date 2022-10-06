78f52a43a01467ee2a245e465036a76cc7b119d8
Thyago Ribeiro <thyagoribeiro608@gmail.com>
2022-10-06 12:42:53 -0400

    Added absolute import configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Header } from "src/components/Header";
+import { Header } from "components/Header";
 import "./styles/main.css";
 
 function App() {
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      src: "src/",
-    },
-  },
+  plugins: [react(), tsconfigPaths()],
 });
